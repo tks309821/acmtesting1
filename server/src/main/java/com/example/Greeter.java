7e1bd3ed5a123cfbb0b3e30698601fded48cb8fa
Keminda3098 <kemindasamaraweera@gmail.com>
2021-07-01 15:33:48 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,3 +17,18 @@ public class Greeter {
     return String.format("Hello, %s!", someone);
   }
 }
+
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
